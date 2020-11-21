4e75f7f6ee97d6cc9c29a39d7c98fd6a7dabcd2e
andres alcocer <andresalcocer7@yahoo.com>
2020-11-20 21:34:27 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Logo from './images/Logo.svg'
+
+function App() {
+  return (
+    <div>
+      <header className='App-header'>
+        <img height='80' src={Logo} alt='React Logo' />
+      </header>
+    </div>
+  )
+}
+
+export default App
