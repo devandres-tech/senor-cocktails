01d4599b0de4f083b6955236feec77318f5cabe8
andres alcocer <andresalcocer7@yahoo.com>
2020-11-26 15:37:12 -0500

    set login background image

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -7,25 +7,26 @@ const Login = (props) => {
   }
 
   return (
-    <div className='container'>
+    <div className='loginContainer-bg'>
       <p onClick={() => props.history.goBack()}>Go Back</p>
-      {console.log('props', props)}
-      <Link to='/'>
-        <img
-          alt='logo'
-          id='logo'
-          src='https://firebasestorage.googleapis.com/v0/b/senor-cocktails.appspot.com/o/Logo.jpg?alt=media&token=db113644-029b-4eb3-9467-32fd50ad84ac'
-        />
-      </Link>
-      <form onSubmit={submitHandler}>
-        <h1>Login</h1>
-        <input type='text' placeholder='email' />
-        <input type='password' placeholder='password' />
-        <button className='btn__primary'>Login</button>
-        <p>
-          Already have an account? <Link to='/signup'>Sign up</Link>
-        </p>
-      </form>
+      <div className='loginContainer container'>
+        <Link to='/'>
+          <img
+            alt='logo'
+            id='logo'
+            src='https://firebasestorage.googleapis.com/v0/b/senor-cocktails.appspot.com/o/Logo.jpg?alt=media&token=db113644-029b-4eb3-9467-32fd50ad84ac'
+          />
+        </Link>
+        <form onSubmit={submitHandler} className='loginContainer__form'>
+          <h1>Login</h1>
+          <input type='text' placeholder='email' />
+          <input type='password' placeholder='password' />
+          <button className='btn__primary'>Login</button>
+          <p>
+            Already have an account? <Link to='/signup'>Sign up</Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
