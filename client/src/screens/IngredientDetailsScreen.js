da7209a42e281c6192be1704c7682541c7e9e7ec
andres alcocer <andresalcocer7@yahoo.com>
2020-12-17 16:06:58 -0500

    set ingredient details screen truncate text functionality

--- client/src/screens/IngredientDetailsScreen.js
+++ client/src/screens/IngredientDetailsScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Container, Row, Col } from 'react-bootstrap'
 
@@ -11,29 +11,56 @@ const IngredientScreen = ({ match }) => {
   const ingredientDetailsState = useSelector((state) => state.ingredientDetails)
   const { loading, error, ingredientDetails } = ingredientDetailsState
 
+  const [expandText, setExpandText] = useState(false)
+
   useEffect(() => {
     dispatch(getIngredientDetails(ingredientId))
   }, [dispatch, ingredientId])
+
+  const truncateText = (text, isExpanded) => {
+    if (text.length > 1000) {
+      return !isExpanded ? text.substring(0, 1000) : text
+    }
+    return text
+  }
+
   return (
     <Container className='ingredientContainer'>
-      <Row>
-        <Col md={4}>
-          <div className='ingredientContainer__left'>
-            <h1>{ingredientDetails.name}</h1>
-            <img src={ingredientDetails.image} alt={'ingredient'} />
-          </div>
-        </Col>
-        <Col md={8}>
-          <div className='ingredientContainer__right'>
-            <div className='typeContent'>
-              <h2>Type</h2>
-              <p>{ingredientDetails.type}</p>
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <Row>
+          <Col md={4}>
+            <div className='ingredientContainer__left'>
+              <h1>{ingredientDetails.name}</h1>
+              <img src={ingredientDetails.image} alt={'ingredient'} />
+              <button className='btn__primary'>Add to my ingredients</button>
             </div>
-            <h2>Description</h2>
-            <p>{ingredientDetails.description}</p>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={8}>
+            <div className='ingredientContainer__right'>
+              {ingredientDetails.type && (
+                <div className='typeContent'>
+                  <h2>Type</h2>
+                  <p>{ingredientDetails.type}</p>
+                </div>
+              )}
+              {ingredientDetails.description && (
+                <>
+                  <h2>Description</h2>
+                  <p>
+                    {truncateText(ingredientDetails.description, expandText)}
+                    {''}
+                    <span onClick={() => setExpandText(!expandText)}>
+                      {!expandText ? '...Read more' : '...Show less'}
+                    </span>
+                  </p>
+                </>
+              )}
+            </div>
+          </Col>
+        </Row>
+      )}
     </Container>
   )
 }
