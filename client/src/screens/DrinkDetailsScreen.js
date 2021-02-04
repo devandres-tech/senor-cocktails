369f0d6b92928cb9d3f37aebd3513b52cca30479
andres alcocer <andresalcocer7@yahoo.com>
2021-02-04 14:30:49 -0500

    set DrinkDetailsScreen component styles

--- client/src/screens/DrinkDetailsScreen.js
+++ client/src/screens/DrinkDetailsScreen.js
@@ -8,20 +8,55 @@ const DrinkDetailsScreen = ({ match, history }) => {
   const { drinkId } = match.params
   const dispatch = useDispatch()
 
+  const drinkDetailsState = useSelector((state) => state.drinkDetails)
+  const { loading, error, drinkDetails } = drinkDetailsState
+
   useEffect(() => {
     dispatch(getDrinkDetails(drinkId))
   }, [dispatch, drinkId])
 
   return (
     <Container className='drinkContainer'>
-      <Row>
-        <Col md={6}>
-          <h1>drink imag</h1>
-        </Col>
-        <Col md={6}>
-          <h1>ingredients</h1>
-        </Col>
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <div className='drinkContainer__leftContent'>
+              <div className='drinkContainer__leftContent header'>
+                <h1>{drinkDetails.name}</h1>
+                <div className='drinkContainer__leftContent rating'>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                </div>
+              </div>
+              <div className='drinkContainer__leftContent image'>
+                <img src={drinkDetails.image} alt='drink' />
+              </div>
+              <button className='btn__primary'>Add to favorites</button>
+              <label>
+                Tried it:
+                <input type='checkbox' name='triedIt' />
+              </label>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className='drinkContainer__rightContent'>
+              <h1>ingredients</h1>
+              {drinkDetails.ingredients &&
+                drinkDetails.ingredients.map((ingredient) => (
+                  <div>
+                    <img src={ingredient.image} alt='ingredient' />
+                    <p>{ingredient.name}</p>
+                    <p>{ingredient.measurement}</p>
+                  </div>
+                ))}
+            </div>
+          </Col>
+        </Row>
+      )}
     </Container>
   )
 }
