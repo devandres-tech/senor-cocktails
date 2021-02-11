fd73297180739068f2410e53ca01b5e6bdeb9b07
andres alcocer <andresalcocer7@yahoo.com>
2021-02-10 22:11:14 -0500

    implemented drinkScreen instructions styling

--- client/src/screens/DrinkDetailsScreen.js
+++ client/src/screens/DrinkDetailsScreen.js
@@ -21,48 +21,62 @@ const DrinkDetailsScreen = ({ match, history }) => {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <Row>
-          <Col md={6}>
-            <div className='drinkContainer__leftContent'>
-              <div className='drinkContainer__leftContent header'>
-                <h1>{drinkDetails.name}</h1>
-                <div className='drinkContainer__leftContent rating'>
-                  <i className='fas fa-star'></i>
-                  <i className='fas fa-star'></i>
-                  <i className='fas fa-star'></i>
-                  <i className='fas fa-star'></i>
+        <>
+          <Row>
+            <Col md={6}>
+              <div className='drinkContainer__leftContent'>
+                <div className='drinkContainer__leftContent header'>
+                  <h1>{drinkDetails.name}</h1>
+                  <div className='drinkContainer__leftContent rating'>
+                    <i className='fas fa-star'></i>
+                    <i className='fas fa-star'></i>
+                    <i className='fas fa-star'></i>
+                    <i className='fas fa-star'></i>
+                  </div>
+                </div>
+                <div className='drinkContainer__leftContent image'>
+                  <img src={drinkDetails.image} alt='drink' />
+                </div>
+                <button className='btn__primary'>Add to favorites</button>
+                <label>
+                  Tried it:
+                  <input type='checkbox' name='triedIt' />
+                </label>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className='drinkContainer__rightContent'>
+                <h2>Ingredients</h2>
+                {drinkDetails.ingredients &&
+                  drinkDetails.ingredients.map((ingredient) => (
+                    <Link to={`/ingredient/${ingredient.ingredient}`}>
+                      <div className='drinkContent__rightContent ingredient'>
+                        <div className='ingredientImage'>
+                          <img src={ingredient.image} alt='ingredient' />
+                        </div>
+                        <p>{ingredient.name}</p>
+                        <p className='ingredientMeasurement'>
+                          {ingredient.measurement}
+                        </p>
+                      </div>
+                    </Link>
+                  ))}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <div className='drinkContainer__comments'>
+                <h2>Directions</h2>
+                <div className='instructions'>
+                  {drinkDetails.instructions && (
+                    <p>{drinkDetails.instructions.EN}</p>
+                  )}
                 </div>
               </div>
-              <div className='drinkContainer__leftContent image'>
-                <img src={drinkDetails.image} alt='drink' />
-              </div>
-              <button className='btn__primary'>Add to favorites</button>
-              <label>
-                Tried it:
-                <input type='checkbox' name='triedIt' />
-              </label>
-            </div>
-          </Col>
-          <Col md={6}>
-            <div className='drinkContainer__rightContent'>
-              <h2>Ingredients</h2>
-              {drinkDetails.ingredients &&
-                drinkDetails.ingredients.map((ingredient) => (
-                  <Link to={`/ingredient/${ingredient.ingredient}`}>
-                    <div className='drinkContent__rightContent ingredient'>
-                      <div className='ingredientImage'>
-                        <img src={ingredient.image} alt='ingredient' />
-                      </div>
-                      <span>{ingredient.name}</span>
-                      <p className='ingredientMeasurement'>
-                        {ingredient.measurement}
-                      </p>
-                    </div>
-                  </Link>
-                ))}
-            </div>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </>
       )}
     </Container>
   )
